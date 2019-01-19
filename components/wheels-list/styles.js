81aa8492c7b58fad29b896467617aa6d3916453c
kengthong <wookengthong@gmail.com>
2019-01-19 22:16:59 +0800

    made changes

--- components/wheels-list/styles.js
+++ components/wheels-list/styles.js
@@ -10,6 +10,29 @@ const styles = StyleSheet.create({
         padding: 4,
         backgroundColor: '#e8e8e8'
     },
+    modalChoiceContainer:{
+        height: 60,
+        width: 240,
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    modalChoiceText:{
+        fontSize: 20,
+        opacity: 0.85
+    }, 
+    modalContainer: {
+        backgroundColor: 'white',
+        borderRadius: 4
+    },  
+    modalWrapper: {
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }, 
     wheelsRowContainer: {
         height: 40,
     }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
