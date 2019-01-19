7aa217ff54b338e174c8a0ef8758acc35d2a0ddf
kengthong <wookengthong@gmail.com>
2019-01-19 12:20:13 +0800

    second commit

---
+++ .bash_profile
@@ -0,0 +1,2 @@
+export ANDROID_SDK=/Users/kengthong/Library/Android/sdk
+export PATH=/Users/myuser/Library/Android/sdk/platform-tools:$PATH
--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -3,9 +3,28 @@ import { Platform } from 'react-native';
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
 import TabBarIcon from '../components/TabBarIcon';
+import MealWheel from '../screens/MealWheel';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+
+const MealWheelStack = createStackNavigator({
+  MealWheel
+})
+
+MealWheel.navigationOptions = {
+  tabBarLabel: 'Meal',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+        ? `ios-information-circle${focused ? '' : '-outline'}`
+        : 'md-information-circle'
+      }
+      />
+  )
+}
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -54,6 +73,7 @@ SettingsStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
+  MealWheelStack,
   HomeStack,
   LinksStack,
   SettingsStack,
