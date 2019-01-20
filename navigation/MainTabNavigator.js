58334ec422654c5852a85028193ea8451b54cf5b
Natalie Loke <natalieloke@Natalies-MacBook-Pro-4.local>
2019-01-20 10:02:11 +0800

    static history page updated

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -9,6 +9,7 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import WheelHome from '../screens/WheelHome';
+import HistoryScreen from '../screens/HistoryScreen';
 
 const WheelsListStack = createStackNavigator({
   WheelsList
@@ -60,19 +61,34 @@ LinksStack.navigationOptions = {
   ),
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+const HistoryStack = createStackNavigator({
+  History: HistoryScreen,
 });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+HistoryStack.navigationOptions = {
+  tabBarLabel: 'History',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === 'ios' ? 'ios-filing' : 'md-filing'}
     />
   ),
 };
+
+
+// const SettingsStack = createStackNavigator({
+//   Settings: SettingsScreen,
+// });
+
+// SettingsStack.navigationOptions = {
+//   tabBarLabel: 'Settings',
+//   tabBarIcon: ({ focused }) => (
+//     <TabBarIcon
+//       focused={focused}
+//       name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+//     />
+//   ),
+// };
 
 const WheelHomeStack = createStackNavigator({
   tabBarLabel: WheelHome,
@@ -97,5 +113,6 @@ export default createBottomTabNavigator({
   WheelsListStack,
   HomeStack,
   LinksStack,
-  SettingsStack,
+  HistoryStack,
+  //SettingsStack,
 });
--- screens/HistoryScreen.js
+++ screens/HistoryScreen.js
@@ -1,27 +1,83 @@
 import React from 'react';
+import { ScrollView, StyleSheet, SectionList, Text, View } from 'react-native';
+
+const data =[
+    {
+        title: '19 Jan 2019',
+        data: [
+            {
+                title: 'The Terrace',
+                data: ['Spicy Marinated Chicken', 
+                        'Teriyaki Chicken Rice',
+                        'Grilled Fish and Pasta']
+            },
+            {
+                title: 'The Deck',
+                data:['Hainanese Chicken Rice', 
+                'Lemon Chicken Rice Set']
+            }                    
+        ]
+    },
+    {
+        title: '18 Jan 2019',
+        data:[
+            {
+                title: 'The Terrace',
+                data: ['Kimchi Fried Rice', 
+                'Teriyaki Salmon Rice']
+            }
+        ]
+    }
+]
 
 export default class HistoryScreen extends React.Component {
-    static navigationOptions = {
-      header: null,
-    };
-    render() {
-        return (
-          <View style={styles.container}>
-            <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-              <HistoryScreenComponent />
-            </ScrollView>
-          </View>
-        );
-      }
-    <SectionList
-    renderItem={({item, index, section}) => <Text key={index}>{item}</Text>}
-    renderSectionHeader={({section: {title}}) => (
-        <Text style={{fontWeight: 'bold'}}>{title}</Text>
-    )}
-    sections={[
-        {title: 'Title1', data: ['item1', 'item2']},
-        {title: 'Title2', data: ['item3', 'item4']},
-        {title: 'Title3', data: ['item5', 'item6']},
-    ]}
-    keyExtractor={(item, index) => item + index}
-    />
+  static navigationOptions = {
+    title: 'History',
+  };
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        <SectionList
+            renderItem={({item, index, section}) => {
+                console.log("item =", item)
+                return (
+                    <View>
+                        <View>
+                            <Text style={{fontSize: 16, backgroundColor: '#99d0fc'}}>
+                                {item.title}
+                            </Text>
+                        </View>
+                        {item.data.map( (food, i) => {
+                            return (
+                                <View key={i}> 
+                                    <Text style ={{fontWeight: 'bold', lineHeight: 40}}>
+                                        {food}
+                                    </Text>
+                                </View>
+                            )
+                        })}
+                    </View>
+                )
+            }}
+            renderSectionHeader={({section: {title}}) => 
+                <View style= {{flexDirection:'row', backgroundColor: 'rgba(247,247,247,1.0)', height: 30, alignItems: 'center'}}> 
+                    <Text style={{fontWeight: '300', lineHeight: 30, fontSize: 20, height: 40}}>{title}</Text>
+                </View>
+            }
+            sections={data}
+            keyExtractor={(item, index) => item + index}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff'
+  },
+});
